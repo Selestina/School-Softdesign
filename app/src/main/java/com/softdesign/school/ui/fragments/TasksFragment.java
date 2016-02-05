e1940f3eaa997c4d59ddaa13ddaaf4cc5af5abab
Selestina <selestina51@gmail.com>
2016-02-05 22:07:12 +0400

    Дз 3

---
+++ app/src/main/java/com/softdesign/school/ui/fragments/TasksFragment.java
@@ -0,0 +1,29 @@
+package com.softdesign.school.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.softdesign.school.R;
+
+
+public class TasksFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View convertView = inflater.inflate(R.layout.fragment_tasks, null, false);
+        getActivity().setTitle(getResources().getString(R.string.drawer_tasks));
+        return convertView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        NavigationView mNavigationView = (NavigationView) getActivity().findViewById(R.id.navigation_view);
+        mNavigationView.getMenu().findItem(R.id.drawer_tasks).setChecked(true);
+    }
+}
